c3e16de02751e98162e97d204a49db0a12e73f51
Punam Kumavat <punamkumavat01@gmail.com>
2023-11-07 12:13:14 +0530

    LOGIN API commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/guesthouses" element={<List/>}/>
         <Route path="/guesthouses/:id" element={<Hotel/>}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+/*import { loginFailed, loginStart, loginSuccess } from '../../context api/Context';
+import './login.css';/* */
+import LoginImg from "../../assets/Login.png";
+import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const {user, loading, error, dispatch } = useContext(AuthContext);
+
+  /*const dispatch = useDispatch();
+  const err = useSelector((state) => state.search.error);*/
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
+
+  }
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"});
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      dispatch({type:"LOGIN_SUCCESS",payload:res.data});
+      navigate("/");
+      document.location.reload();
+    } catch (error) {
+      dispatch({type:"LOGIN_FAILURE",payload:error.response.data});
+    }
+  }
+  console.log(user);
+  return (
+    <div className="main-login">
+      <div className="login">
+        <div className="left-login">
+          <img src={LoginImg} alt="..." />
+        </div>
+        <div className="right-login">
+          <h2>Sign in or create an account</h2>
+          <input
+            type="text"
+            id="username"
+            className="login-input"
+            placeholder="Username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            id="password"
+            className="login-input"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            style={{ width: "18rem", margin: "0" }}
+            className="btn"
+            onClick={handleClick}
+            id="liveAlertBtn"
+          >
+            Submit{" "}
+          </button>
+
+          {error && <span className="err">{error} </span>}
+          <div className="signtxt">
+            By signing in or creating an account, you agree with our{" "}
+            <span>Terms & conditions</span> and <span>Privacy statement</span>
+          </div>
+          <hr />
+          <div className="copyright">
+            All rights reserved. Copyright (2006 - 2023) - Booking.com™
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
